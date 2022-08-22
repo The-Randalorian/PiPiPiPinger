7a503f201b26cb954addfc56be2ab682d3a51e47
David Johnston <8741983+The-Randalorian@users.noreply.github.com>
2022-08-22 11:15:29 -0400

    Initial Code Upload
    Signed-off-by: David Johnston <8741983+The-Randalorian@users.noreply.github.com>

--- defaultconfig.py
+++ defaultconfig.py
@@ -2,25 +2,25 @@
 
 # E-Mail Settings
 port = 465  # For SSL
-smtp_server = "smtp.sample.com"
+smtp_server = "smtp.sample.com"  # Enter the address of the smtp server
 sender_email = "sender@sample.com"  # Enter your address
 receiver_emails = "receiver1@sample.com, receiver2@sample.com"  # Enter receiver addresses separated by ", "
-password = "samplepassword123"
+password = "samplepassword123"  # password for the email server
 alert_message = """\
 Subject: Device Unreachable
 
 The device is currently unable to be reached.
-"""
+"""  # Message to send when the device cannot be reached
 clear_message = """\
 Subject: Device Now Reachable
 
 The device is now able to be reached.
-"""
+"""  # Message to send when the device can be reached again
 
 # Ping Settings
-target = "127.0.0.1"
-delay = 300
-threshold = 0.8
+target = "127.0.0.1"  # IP of the device
+delay = 300  # time to wait between pings
+threshold = 0.8  # what ratio of pings must fail for the device to be considered unreachable
 
 # Error settings
 error_receiver_emails = "error@sample.com"
